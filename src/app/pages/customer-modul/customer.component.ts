b45f0a7b966271549705bd9dcee6f70983c92062
Ghait <ghait@mogic.com>
2019-12-22 23:32:45 +0100

    SEPRO-1: Test  for Jira

--- src/app/pages/customer-modul/customer.component.ts
+++ src/app/pages/customer-modul/customer.component.ts
@@ -3,9 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ngx-app-customer',
   styleUrls: ['./customer.component.scss'],
-  template: `
-    <router-outlet></router-outlet>
-  `,
+  template: `<router-outlet></router-outlet>`,
 })
 export class CustomerComponent implements OnInit {
 
